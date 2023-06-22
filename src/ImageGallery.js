6c35ec9dbe73bb6e16e2dcef23941b8deb9f8d9f
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-06-22 18:29:24 +0530

    Update ImageGallery.js

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,16 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchImages } from './actions';
-import './ImageGallery.css'; // Import the CSS file
+import './ImageGallery.css';
 
 const ImageGallery = () => {
   const images = useSelector((state) => state.images);
-
   const dispatch = useDispatch();
+  const [imageCount, setImageCount] = useState(1); 
 
   useEffect(() => {
     dispatch(fetchImages());
   }, [dispatch]);
+
+  const handleIncrement = () => {
+    setImageCount(imageCount + 1);
+  };
+
+  const handleDecrement = () => {
+    if (imageCount > 0) {
+      setImageCount(imageCount - 1);
+    }
+  };
 
   if (images.length === 0) {
     return <div>Loading...</div>;
@@ -18,7 +28,12 @@ const ImageGallery = () => {
 
   return (
     <div className="image-gallery-container">
-      {images.slice(0, 8).map((image, index) => (
+      <div className="image-gallery-controls">
+        <button onClick={handleDecrement}>-</button>
+        <span>{imageCount}</span>
+        <button onClick={handleIncrement}>+</button>
+      </div>
+      {images.slice(0, imageCount).map((image, index) => (
         <div key={index} className="image-gallery-item">
           <img src={image} alt={`Dog ${index + 1}`} />
         </div>
